971db2d4d60819ccf2215e9ed4482acb48d507ff
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-12-11 21:14:36 +0200

    update code

--- qb-dumpsters/client/client.lua
+++ qb-dumpsters/client/client.lua
@@ -2,17 +2,25 @@ local searched = {3423423424}
 local canSearch = true
 local searchTime = 14000
 
-RegisterNetEvent('pixellife:removeDumpster')
-AddEventHandler('pixellife:removeDumpster', function(object)
+local function startSearching(time,cb)
+    canSearch = false
+    TriggerEvent('animations:client:EmoteCommandStart', {Config.CollectingEmoteName})
+    exports.rprogress:Start('Checking Dumpster..', 8000)
+    TriggerEvent('animations:client:EmoteCommandStart', {"C"}) 
+    ClearPedTasks(PlayerPedId())
+    canSearch = true
+    TriggerServerEvent(cb)
+end
+
+RegisterNetEvent('pixellife:removeDumpster', function(object)
     for i = 1, #searched do
         if searched[i] == object then
-            table.remove(searched, i)
+            searched[i] = nil
         end
     end
 end)
 
-RegisterNetEvent('qb-dumpster:client:eye')
-AddEventHandler('qb-dumpster:client:eye', function(data)
+RegisterNetEvent('qb-dumpster:client:eye', function(data)
     local dumpsterFound = false
     if canSearch then
         for i = 1, #searched do 
@@ -24,20 +32,9 @@ AddEventHandler('qb-dumpster:client:eye', function(data)
             elseif i == #searched and not dumpsterFound then 
                 QBCore.Functions.Notify('You begin to search the dumpster..', "success") 
                 startSearching(searchTime, 'pixellife:giveDumpsterReward') 
-                TriggerServerEvent('pixellife:startDumpsterTimer', data.entity) 
-                table.insert(searched, data.entity) 
+                TriggerServerEvent('pixellife:startDumpsterTimer', data.entity)
+                searched[#searched+1] = data.entity
             end 
         end
     end
 end)
-
--- Functions
-function startSearching(time,cb)
-    canSearch = false
-    TriggerEvent('animations:client:EmoteCommandStart', {Config.CollectingEmoteName})
-    exports.rprogress:Start('Checking Dumpster..', 8000)
-    TriggerEvent('animations:client:EmoteCommandStart', {"C"}) 
-    ClearPedTasks(PlayerPedId())
-    canSearch = true
-    TriggerServerEvent(cb)
-end
